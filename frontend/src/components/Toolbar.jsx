b2babed503308ebfea83b849c5c41ad2c1bcf987
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2022-09-02 22:20:18 +0200

    feat : updated toolbar

--- frontend/src/components/Toolbar.jsx
+++ frontend/src/components/Toolbar.jsx
@@ -10,37 +10,30 @@ import {
 const Toolbar = () => {
   return (
     <div class="toolbar grid grid-cols-2 bg-white/[0.05] backdrop-blur-sm pl-10 pr-10 pt-1 pb-1">
-      <div>
-        <ul className="flex">
-          <li className="mr-6 text-white">
-            <img src={logo} alt="logo" style={{ height: "20px" }} />
-          </li>
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <h4>Home</h4>
-          </li>
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <h4>Library</h4>
-          </li>
-        </ul>
+      <div className="flex align-middle">
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <h4>Home</h4>
+        </div>
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <h4>library</h4>
+        </div>
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <h4>Settings</h4>
+        </div>
       </div>
-      <div>
-        <ul className="flex justify-end	">
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <BsArrowLeftCircle />
-          </li>
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <BsArrowRightCircle />
-          </li>
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <BsSearch />
-          </li>
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <BsVolumeDown />
-          </li>
-          <li className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
-            <h4>Settings</h4>
-          </li>
-        </ul>
+      <div className="flex justify-end	items-center">
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <BsArrowLeftCircle />
+        </div>
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <BsArrowRightCircle />
+        </div>
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <BsSearch />
+        </div>
+        <div className="mr-10 text-white hover:text-white/60 hover:cursor-pointer">
+          <BsVolumeDown />
+        </div>
       </div>
     </div>
   );
